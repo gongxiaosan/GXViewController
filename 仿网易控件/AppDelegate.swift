0f2be75ca0d1408b92abaed041b3ce204f2c422a
gongxiao <gongxiao@gongxiaodeMBP.lan>
2016-06-20 15:13:13 +0800

    new

--- 仿网易控件/AppDelegate.swift
+++ 仿网易控件/AppDelegate.swift
@@ -27,7 +27,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
 
         self.window?.rootViewController = UINavigationController(rootViewController: GXViewController(subControllers: vcs))
-        
         self.window?.makeKeyAndVisible()
         return true
     }
